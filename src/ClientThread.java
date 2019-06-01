24eb9f5f3d18152f11278816194a46fc31387520
trew26 <spm@instant.de>
2019-06-01 11:25:42 +0200

    [INTERNAL] Added multiple write command for each thread

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -1,5 +1,6 @@
 public class ClientThread extends Thread {
 
+    PersistenceManager pm = PersistenceManager.getInstance();
     Integer number;
 
     public ClientThread(Integer number) {
@@ -8,7 +9,27 @@ public class ClientThread extends Thread {
 
     public void run() {
         while(true) {
-            System.out.println(number);
+
+            int taid = pm.beginTransaction();
+            System.out.println(taid);
+            //   pm.write();
+            pm.write(1, taid, "first");
+            try {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e) {
+                return;
+            }
+            System.out.println(taid);
+            pm.write(1, taid, "second");
+            try {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e) {
+                return;
+            }
+            System.out.println(taid);
+            pm.write(1, taid, "third");
             try {
                 Thread.sleep(2000);
             }
